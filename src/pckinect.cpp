1ee7185a76e049e78189b01be11335b3868bed1d
Dietrich Epp <depp@zdome.net>
2015-01-29 12:33:16 -0800

    Record point cloud with depth key

--- src/pckinect.cpp
+++ src/pckinect.cpp
@@ -1,12 +1,15 @@
 #include "defs.hpp"
 
-#include "libfreenect.h"
-#include "libfreenect_sync.h"
+#include <unistd.h>
+
 #include <cmath>
 #include <cstdint>
 #include <cstdio>
 #include <string>
 #include <vector>
+
+#include "libfreenect.h"
+#include "libfreenect_sync.h"
 
 const int WIDTH = 640;
 const int HEIGHT = 480;
@@ -30,17 +33,88 @@ unsigned read_color(const unsigned char *p) {
     return c.ui;
 }
 
+const unsigned short *get_depth() {
+    void *p;
+    uint32_t ts;
+    int r;
+    r = freenect_sync_get_depth(
+        &p, &ts, 0, FREENECT_DEPTH_REGISTERED);
+    if (r < 0) {
+        die("Could not get depth data.");
+    }
+    return static_cast<const unsigned short *>(p);
+}
+
+const unsigned char *get_color() {
+    void *p;
+    uint32_t ts;
+    int r;
+    r = freenect_sync_get_video(
+        &p, &ts, 0, FREENECT_VIDEO_RGB);
+    if (r < 0) {
+        die("Could not get color data.");
+    }
+    return static_cast<const unsigned char *>(p);
+}
+
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        die("Usage: pckinect FILE FRAME_COUNT");
+    if (argc != 4) {
+        die("Usage: pckinect FILE FRAME_COUNT KEY_DISTANCE_MM");
     }
 
     int frame_count = std::stoi(argv[2]);
     if (frame_count < 1) {
         die("Frame count is negative.");
     }
+    int key_distance = std::stoi(argv[3]);
+    if (key_distance <= 0 || key_distance > 1000) {
+        die("Key distance must be positive and no more than 1000.");
+    }
 
-    std::fprintf(stderr, "Writing data to %s.", argv[1]);
+    std::fputs("Creating depth key.\n", stderr);
+    std::vector<unsigned short> depth_key(WIDTH * HEIGHT);
+    {
+        const unsigned short *depth = get_depth();
+
+        // Fill holes by taking maximum nearby value
+        std::fputs("    Filling holes.\n", stderr);
+        int holes = 0;
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
+                unsigned short d = depth[y * WIDTH + x];
+                if (!d) {
+                    d = 0xffff;
+                }
+                for (int r = 1; d == 0xffff && r < 64; r++) {
+                    int y0 = std::max(y - r, 0);
+                    int y1 = std::min(y + r + 1, HEIGHT);
+                    int x0 = std::max(x - r, 0);
+                    int x1 = std::min(x + r + 1, WIDTH);
+                    for (int yy = y0; yy < y1; yy++) {
+                        for (int xx = x0; xx < x1; xx++) {
+                            unsigned short dd = depth[yy * WIDTH + xx];
+                            if (!dd) {
+                                continue;
+                            } else if (dd < d) {
+                                d = dd;
+                            }
+                        }
+                    }
+                }
+                if (d == 0xffff) {
+                    d = 0;
+                    holes++;
+                }
+                depth_key[y * WIDTH + x] = d;
+            }
+        }
+        std::fprintf(stderr, "   Could not fill %d pixels.\n", holes);
+    }
+
+    std::fputs("Sleeping 2 seconds.\n", stderr);
+    sleep(2);
+
+    std::fprintf(stderr, "Writing data to %s.\n", argv[1]);
     FILE *fp = std::fopen(argv[1], "wb");
     if (!fp) {
         die("Could not open file.");
@@ -48,35 +122,17 @@ int main(int argc, char *argv[]) {
 
     std::vector<Point> points;
     for (int i = 0; i < frame_count; i++) {
-        const unsigned short *depth;
-        const unsigned char *color;
-
-        // Read data from Kinect
-        {
-            void *p;
-            uint32_t ts;
-            int r;
-            r = freenect_sync_get_depth(
-                &p, &ts, 0, FREENECT_DEPTH_REGISTERED);
-            if (r < 0) {
-                die("Could not get depth data.");
-            }
-            depth = static_cast<const unsigned short *>(p);
-            r = freenect_sync_get_video(
-                &p, &ts, 0, FREENECT_VIDEO_RGB);
-            if (r < 0) {
-                die("Could not get color data.");
-            }
-            color = static_cast<const unsigned char *>(p);
-        }
+        const unsigned short *depth = get_depth();
+        const unsigned char *color = get_color();
 
         // Convert data to points.
         points.clear();
         for (int y = 0; y < HEIGHT; y++) {
             for (int x = 0; x < WIDTH; x++) {
                 int d = depth[y*WIDTH+x];
+                int dk = depth_key[y*WIDTH+x];
                 unsigned c = read_color(color + (y * WIDTH + x) * 3);
-                if (!d) {
+                if (!d || d >= dk - key_distance) {
                     continue;
                 }
                 float z = 0.001f * d;
